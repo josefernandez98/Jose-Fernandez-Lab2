d3a51e5860336c603e603be8ac371d77ad527c16
Jose Fernandez <jrfo_04@unitec.edu>
2016-10-21 13:27:48 -0600

    Se creo el menu

---
+++ lab.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>;
+using namespace std;
+
+int main() 
+{
+	int opcion;
+	do 
+	{
+		cout << "---Menu---" endl;
+		cout << "1. Ejercicio 1.";
+		cout << "2. Ejercicio 2.";
+		cout << "3. Ejercicio 3.";
+		cout << "4. Salir.";
+		cout << "Ingrese su opcion:"
+		cin >> opcion;
+		if (opcion == 1) {
+			
+
+
+		}//Fin del if
+		if (opcion == 2) {
+
+
+
+		}//Fin del if
+		if (opcion == 3) {
+
+
+		}//Fin del if
+	} while (opcion != 4);
+	return 0;
+}//Fin del main
