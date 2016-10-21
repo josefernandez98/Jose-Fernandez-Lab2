6c3c1748613f554710e3ac81ad07795816bb21af
Jose Fernandez <jrfo_04@unitec.edu>
2016-10-21 15:33:11 -0600

    Se consiguen los divisores

--- lab.cpp
+++ lab.cpp
@@ -50,7 +50,33 @@ int main(int argc, char const *argv[])
 			cout << cadena << " esta en base " << basePan << " y equivale a " << baseDec << " en decimal" << endl;
 		}//Fin del i
 		if (opcion == 2) {
+			int numero;
+			int resultadoFinal;
+			cout << "Ingrese un numero:";
+			cin >> numero;
+			int contador = 0;
+			for (int i = 1; i <= numero; ++i)
+			{
+				if (numero % i == 0) {
+					contador++;
+				}//Fin del if
+			}//Fin del for
+			int divisores[contador];
+			int resultadoDivisiones[contador];
+			int size = 0;
+			for (int i = 1; i <= numero; ++i)
+			{
+				if (numero % i == 0) {
+					divisores[size] = i;
+					size++;
+				}//Fin del if	
+			}//Fin del for
 
+			for (int i = 0; i < size; ++i)
+			{
+				resultadoDivisiones[i] = (divisores[i] + numero)/divisores[i];
+			}//Fin del for
+			imprimirArreglo(resultadoDivisiones, size);
 
 
 		}//Fin del if
@@ -61,3 +87,11 @@ int main(int argc, char const *argv[])
 	} while (opcion != 4);
 	return 0;
 }//Fin del main
+
+void imprimirArreglo (int arreglo[], int a) {
+	for (int i = 0; i < a; ++i)
+	{
+		cout << arreglo[i] << ",";
+	}
+	cout << endl;
+} 
